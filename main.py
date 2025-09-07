b383af240d7e2b9ac9d131a887860e5f0c0118b5
Winardi <winardialong123@gmail.com>
2025-09-07 20:21:16 +0800

    Add features

--- main.py
+++ main.py
@@ -23,51 +23,72 @@ session_duration = 20            # Seconds
 session_count = 1
 scheduler = BackgroundScheduler()
 
-def Notify():
+# Constants
+WORKTIME = int(0.8*session_duration)
+RESTTIME = int(0.2*session_duration)
+
+def Notify(msg):
     notification.notify(
         title="Welcome to FocusPal",
-        message="Start Deep Work",
+        message=msg,
         app_name="FocusPal",
         timeout=10,
         ticker="FP1.0"
     )
 
-def StartDeepWork():
+def StartDeepWork() -> int:
     global session_count
 
-    scheduler.add_job(Notify, 'interval', seconds=5, id='notify')
+    scheduler.add_job(Notify, 'interval', args=["Start Deep Work"], seconds=10, id='notify')
 
     print(f"Start deep work session #{session_count}? (Y/N)")
     response = input()
 
 
     if (response.lower() == 'y'):
-        start_time = datetime.now()
-        print("Start time: ", start_time.strftime("%H:%M:%S"))
-
         scheduler.remove_job('notify')
 
-        # while True:
-        #     stop_time = datetime.now()
-        #     elapsed_time = stop_time - start_time
+        # Deep Work Session
+        start_time = datetime.now()
+        print("Start time: ", start_time.strftime("%H:%M:%S"))
+        with tqdm(total=WORKTIME) as pbar:
+            # Cannot put the print statement between tqdm - update() else the progress bar will be splitted
+            while True:
+                elapsed = int((datetime.now() - start_time).total_seconds())
+                pbar.update(elapsed - pbar.n)
+                # pbar.n = elapsed
+                # pbar.refresh()
 
-        #     if (elapsed_time.total_seconds() >= 10):
-        #         print("Session Completed!")
-        #         break
+                if (elapsed == WORKTIME):
+                    break
 
-        #     time.sleep(5)
+                time.sleep(1)
 
-        for i in tqdm(range(session_duration)):
-            time.sleep(1)
+        # Rest Session
+        start_time = datetime.now()
+        print("Take a break!")
+        scheduler.add_job(Notify, 'date', args=["Take a short break!"], run_date=datetime.now(), id='notify_rest')
+        with tqdm(total=RESTTIME) as pbar:
+            while True:
+                elapsed = int((datetime.now() - start_time).total_seconds())
+                pbar.update(elapsed - pbar.n)
+
+                if (elapsed == RESTTIME):
+                    scheduler.add_job(Notify, 'date', args=["Session Completed!"], run_date=datetime.now(), id='notify_end')
+                    break
 
         stop_time = datetime.now()
         print("End time: ", stop_time.strftime("%H:%M:%S"))
         print("Session Completed!")
         session_count += 1
+        return 0
 
     elif (response.lower() == 'n'):
         scheduler.remove_all_jobs()
         print("Session not started.")
+    
+    elif (response.lower() == 'exit'):
+        return 1
     
 
 def main():
@@ -75,7 +96,10 @@ def main():
     # scheduler.add_job(StartDeepWork, 'interval', seconds=5)
     scheduler.start()
     while True:
-        StartDeepWork()
+        if (StartDeepWork() == 1):
+            scheduler.remove_all_jobs()
+            break
+        
     # scheduler.start()
 
 
